997d09952f6f80677fea5cddc80f13bc497037da
Sebastian Fellner <ga96kug@tum.de>
2018-09-13 10:56:13 +0200

    refactoring

--- vae.py
+++ vae.py
@@ -3,9 +3,7 @@ from matplotlib import pyplot
 from tensorflow.examples.tutorials.mnist import input_data
 import os
 
-# from utils import *
-# from ops import *
-from ops import *
+import tensorflow as tf
 
 
 # this code is mainly copied from:
@@ -19,53 +17,68 @@ class LatentAttention():
         self.n_z = 20
         self.batchsize = 100
 
+        # *** FEED FORWARD
+        # encoder
         self.images = tf.placeholder(tf.float32, [None, 784])
         image_matrix = tf.reshape(self.images, [-1, 28, 28, 1])
-        z_mean, z_stddev = self.recognition(image_matrix)
+        z_mean, z_stddev = self.encode(image_matrix)
+
+        # generate samples
         samples = tf.random_normal([self.batchsize, self.n_z], 0, 1, dtype=tf.float32)
         guessed_z = z_mean + (z_stddev * samples)
 
-        self.generated_images = self.generation(guessed_z)
+        # decoder
+        self.generated_images = self.decode(guessed_z)
         generated_flat = tf.reshape(self.generated_images, [self.batchsize, 28 * 28])
 
+        # *** LOSS
+        # one 2 one loss (input/output)
         self.generation_loss = -tf.reduce_sum(
             self.images * tf.log(1e-8 + generated_flat) + (1 - self.images) * tf.log(1e-8 + 1 - generated_flat), 1)
 
+        # KL-divergence
         self.latent_loss = 0.5 * tf.reduce_sum(
             tf.square(z_mean) + tf.square(z_stddev) - tf.log(tf.square(z_stddev)) - 1, 1)
+
+        # total loss
         self.cost = tf.reduce_mean(self.generation_loss + self.latent_loss)
+
+        # *** OPTIMIZER
         self.optimizer = tf.train.AdamOptimizer(0.001).minimize(self.cost)
 
     # encoder
-    def recognition(self, input_images):
-        with tf.variable_scope("recognition"):
-            h1 = lrelu(conv2d(input_images, 1, 16, "d_h1"))  # 28x28x1 -> 14x14x16
-            h2 = lrelu(conv2d(h1, 16, 32, "d_h2"))  # 14x14x16 -> 7x7x32
+    def encode(self, input_images):
+        with tf.variable_scope("encode"):
+            # 28x28x1 -> 14x14x16
+            h1 = tf.layers.conv2d(input_images, 16, kernel_size=5, activation=tf.nn.leaky_relu, strides=2,
+                                  padding='same')
+            # 14x14x16 -> 7x7x32
+            h2 = tf.layers.conv2d(h1, 32, kernel_size=5, activation=tf.nn.leaky_relu, strides=2, padding='same')
+            # 7x7x32 -> (7*7*32)
             h2_flat = tf.reshape(h2, [self.batchsize, 7 * 7 * 32])
 
-            w_mean = dense(h2_flat, 7 * 7 * 32, self.n_z, "w_mean")
-            w_stddev = dense(h2_flat, 7 * 7 * 32, self.n_z, "w_stddev")
-
+            w_mean = tf.layers.dense(h2_flat, self.n_z)
+            w_stddev = tf.layers.dense(h2_flat, self.n_z)
         return w_mean, w_stddev
 
     # decoder
-    def generation(self, z):
-        with tf.variable_scope("generation"):
-            z_develop = dense(z, self.n_z, 7 * 7 * 32, scope='z_matrix')
-            z_matrix = tf.nn.relu(tf.reshape(z_develop, [self.batchsize, 7, 7, 32]))
-            h1 = tf.nn.relu(conv_transpose(z_matrix, [self.batchsize, 14, 14, 16], "g_h1"))
-            h2 = conv_transpose(h1, [self.batchsize, 28, 28, 1], "g_h2")
+    def decode(self, z):
+        with tf.variable_scope("decode"):
+            z_develop = tf.layers.dense(z, 7 * 7 * 32, activation=tf.nn.leaky_relu)
+            z_matrix = tf.reshape(z_develop, [self.batchsize, 7, 7, 32])
+            h1 = tf.layers.conv2d_transpose(z_matrix, 16, kernel_size=5, activation=tf.nn.leaky_relu, strides=2,
+                                            padding='same')
+            h2 = tf.layers.conv2d_transpose(h1, 1, kernel_size=5, activation=tf.nn.leaky_relu,
+                                            strides=2, padding='same')
             h2 = tf.nn.sigmoid(h2)
 
         return h2
 
     def train(self):
-        if not os.path.exists("/training"):
-            os.makedirs("/training")
+        if not os.path.exists("./training"):
+            os.makedirs("./training")
 
         visualization = self.mnist.train.next_batch(self.batchsize)[0]
-        reshaped_vis = visualization.reshape(self.batchsize, 28, 28)
-        # ims("results/base.jpg", merge(reshaped_vis[:64], [8, 8]))
         # train
         saver = tf.train.Saver(max_to_keep=2)
         with tf.Session() as sess:
@@ -83,7 +96,6 @@ class LatentAttention():
                 saver.save(sess, os.getcwd() + "/training/train", global_step=epoch)
                 generated_test = sess.run(self.generated_images, feed_dict={self.images: visualization})
                 generated_test = generated_test.reshape(self.batchsize, 28, 28)
-                # ims("results/" + str(epoch) + ".jpg", merge(generated_test[:64], [8, 8]))
                 pyplot.imsave(str(epoch) + ".jpg", generated_test[0])
 
 
