d4149731ac2e6f9b28b132810e96d7dc8b5e45d7
Sebastian Fellner <ga96kug@tum.de>
2018-09-13 16:05:22 +0200

    added image logging to tensorboard

--- config/config.py
+++ config/config.py
@@ -4,9 +4,9 @@ from collections import namedtuple
 
 from data.DataProvider import DataProvider
 
-model_args = namedtuple("model_args", "data_set, hidden_size, batch_size")
+model_args = namedtuple("model_args", "data_set, hidden_size, batch_size, learning_rate")
 
-DEFAULT = model_args(data_set=DataProvider.get_mnist, hidden_size=20, batch_size=128)
+DEFAULT = model_args(data_set=DataProvider.get_mnist, hidden_size=20, batch_size=128, learning_rate=.001)
 
 TIME = str(datetime.datetime.now())
 
--- models/Vae.py
+++ models/Vae.py
@@ -10,7 +10,7 @@ from config.config import TRAINING, TF_BOARD, IMAGES
 
 class VAE(ABC):
 
-    def __init__(self, data_set, hidden_size, batch_size):
+    def __init__(self, data_set, hidden_size, batch_size, learning_rate):
         self.data_set, self.width = data_set()
         self.batch_size = batch_size
         self.hidden_size = hidden_size
@@ -18,9 +18,9 @@ class VAE(ABC):
 
         # *** FEED FORWARD
         # encoder
-        self.images = tf.placeholder(tf.float32, [None, 784])
-        image_matrix = tf.reshape(self.images, [-1, 28, 28, 1])
-        z_mean, z_stddev = self.encode(image_matrix)
+        self.images_1d = tf.placeholder(tf.float32, [None, self.width ** 2])
+        images_2d = tf.reshape(self.images_1d, [-1, self.width, self.width, 1])
+        z_mean, z_stddev = self.encode(images_2d)
 
         # generate samples
         samples = tf.random_normal([self.batch_size, self.hidden_size], 0, 1, dtype=tf.float32)
@@ -32,7 +32,7 @@ class VAE(ABC):
 
         # *** LOSS
         # pixel correspondence loss (input/output)
-        self.generation_loss = tf.abs(tf.reduce_sum(tf.square(self.images - generated_flat))) / self.batch_size
+        self.generation_loss = tf.abs(tf.reduce_sum(tf.square(self.images_1d - generated_flat))) / self.batch_size
 
         # KL-divergence
         self.latent_loss = 0.5 * tf.reduce_sum(
@@ -42,7 +42,7 @@ class VAE(ABC):
         self.cost = tf.reduce_mean(self.generation_loss + self.latent_loss)
 
         # *** OPTIMIZER
-        self.optimizer = tf.train.AdamOptimizer(0.001).minimize(self.cost)
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.cost)
 
         self.init = tf.global_variables_initializer()
 
@@ -51,7 +51,8 @@ class VAE(ABC):
         tf.summary.scalar("generation", tf.reduce_mean(self.generation_loss), family="loss")
         tf.summary.scalar("KL-Divergence", tf.reduce_mean(self.latent_loss), family="loss")
         self.merged_summary_op = tf.summary.merge_all()
-
+        self.image_summary = tf.summary.image("image", tf.reshape(self.generated_images,
+                                                                  (-1, self.width, self.width, 1)))
         os.environ["Model"] = self.__class__.__name__
 
     @abstractmethod
@@ -70,7 +71,7 @@ class VAE(ABC):
     def train(self):
         self.create_folders()
 
-        static_batch = self.data_set.validation.next_batch(
+        static_batch = self.data_set.test.next_batch(
             self.batch_size)[0]
         summary_writer = tf.summary.FileWriter(TF_BOARD(), graph=tf.get_default_graph())
 
@@ -86,42 +87,48 @@ class VAE(ABC):
                     batch = self.data_set.train.next_batch(self.batch_size)[0]
                     _, gen_loss, lat_loss, summary = sess.run(
                         (self.optimizer, self.generation_loss, self.latent_loss, self.merged_summary_op),
-                        feed_dict={self.images: batch})
+                        feed_dict={self.images_1d: batch})
 
                     # Write logs at every iteration
                     summary_writer.add_summary(summary, epoch * self.iterations_per_epoch + idx)
 
+                # todo get these values from last summary
                 print("epoch {}: genloss {} latloss {}".format(epoch, np.mean(gen_loss), np.mean(lat_loss)))
+
+                # save current model
                 saver.save(sess, TRAINING() + "/train", global_step=epoch * self.data_set.train.num_examples)
-                generated_test = sess.run(self.generated_images, feed_dict={self.images: static_batch})
-                generated_test = generated_test.reshape(self.batch_size, 28, 28)
-                pyplot.imsave(IMAGES() + "/{0:03d}.jpg".format(epoch), generated_test[0])
+
+                # log images
+                generated_images, images_summary = sess.run((self.generated_images, self.image_summary),
+                                                            feed_dict={self.images_1d: static_batch})
+                summary_writer.add_summary(images_summary,
+                                           global_step=epoch * self.data_set.train.num_examples)
 
 
 class SimpleVAE(VAE):
-
     def encode(self, input_images):
         with tf.variable_scope("encode"):
             # 28x28x1 -> 14x14x16
-            h1 = tf.layers.conv2d(input_images, 16, kernel_size=5, activation=tf.nn.leaky_relu, strides=2,
-                                  padding='same')
+            conv_1 = tf.layers.conv2d(input_images, 16, kernel_size=5, activation=tf.nn.leaky_relu, strides=2,
+                                      padding='same')
             # 14x14x16 -> 7x7x32
-            h2 = tf.layers.conv2d(h1, 32, kernel_size=5, activation=tf.nn.leaky_relu, strides=2, padding='same')
+            conv_2 = tf.layers.conv2d(conv_1, 32, kernel_size=5, activation=tf.nn.leaky_relu, strides=2, padding='same')
             # 7x7x32 -> (7*7*32)
-            h2_flat = tf.reshape(h2, [self.batch_size, 7 * 7 * 32])
+            flatten = tf.reshape(conv_2, [self.batch_size, 7 * 7 * 32])
 
-            w_mean = tf.layers.dense(h2_flat, self.hidden_size)
-            w_stddev = tf.layers.dense(h2_flat, self.hidden_size)
-        return w_mean, w_stddev
+            mean = tf.layers.dense(flatten, self.hidden_size)
+            stddev = tf.layers.dense(flatten, self.hidden_size)
+        return mean, stddev
 
     def decode(self, z):
         with tf.variable_scope("decode"):
-            z_develop = tf.layers.dense(z, 7 * 7 * 32, activation=tf.nn.leaky_relu)
-            z_matrix = tf.reshape(z_develop, [self.batch_size, 7, 7, 32])
-            h1 = tf.layers.conv2d_transpose(z_matrix, 16, kernel_size=5, activation=tf.nn.leaky_relu, strides=2,
-                                            padding='same')
-            h2 = tf.layers.conv2d_transpose(h1, 1, kernel_size=5, activation=tf.nn.leaky_relu,
-                                            strides=2, padding='same')
-            h2 = tf.nn.sigmoid(h2)
+            starting_res_1d = tf.layers.dense(z, 7 * 7 * 32, activation=tf.nn.leaky_relu)
+            starting_res_2d = tf.reshape(starting_res_1d, [self.batch_size, 7, 7, 32])
+            conv_1T = tf.layers.conv2d_transpose(starting_res_2d, 16, kernel_size=5, activation=tf.nn.leaky_relu,
+                                                 strides=2,
+                                                 padding='same')
+            conv_2T = tf.layers.conv2d_transpose(conv_1T, 1, kernel_size=5, activation=tf.nn.leaky_relu,
+                                                 strides=2, padding='same')
+            sig = tf.nn.sigmoid(conv_2T)
 
-        return h2
+        return sig
