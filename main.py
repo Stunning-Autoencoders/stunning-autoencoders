3029ab6ef63b5a509adf8841ea69720f850a02be
mathsten <stensrudmathias@gmail.com>
2018-09-27 15:50:16 +0200

    Added example of usage of method "generate_image_from_audio" to main.py

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 if __name__ == '__main__':
-    SimpleVAE(*DEFAULT).train()
+    # SimpleVAE(*DEFAULT).train()
     vae = SimpleVAE(*DEFAULT)
-    vae.load_pretrained("./training/SimpleVAE/run3")
+    # vae.load_pretrained("./training/SimpleVAE/run3")
+    vae.generate_images_from_audio(weights="./training/SimpleVAE/run3",
+                                   file="music/heartbeat.wav",
+                                   interval=2,
+                                   samples=20)
+
