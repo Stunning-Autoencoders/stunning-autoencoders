18072668c6a528a658c220b1df011bcaaf977dab
mathsten <stensrudmathias@gmail.com>
2018-10-15 15:33:37 +0200

    Added generate_mell()

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 if __name__ == '__main__':
-    SimpleVAE(*DEFAULT).train()
-    vae = SimpleVAE(*DEFAULT)
-    #vae.load_pretrained("./training/SimpleVAE/run2")
+    # SimpleVAE(*DEFAULT).train()
+    # vae = SimpleVAE(*DEFAULT)
+    # vae.load_pretrained("./training/SimpleVAE/run3")
 
-    #create_video(SimpleVAE, DEFAULT, "./music/heartbeat.wav", "./training/SimpleVAE/run3",
-                 #"heartbeat.mp4", fps=24)
+    create_video(SimpleVAE, DEFAULT, "./music/sweep_20Hz_20000Hz_-3dBFS_10s.wav", "./training/SimpleVAE/run0",
+                 "frequency_mell.mp4", fps=24)
