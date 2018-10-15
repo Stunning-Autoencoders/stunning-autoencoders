cf7289113ca5004299d2b03122a15a621eae311b
radufb <raduburceaflorin@gmail.com>
2018-10-15 02:47:25 -0700

    With celebA function

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 if __name__ == '__main__':
-    #SimpleVAE(*DEFAULT).train()
-    #vae = SimpleVAE(*DEFAULT)
+    SimpleVAE(*DEFAULT).train()
+    vae = SimpleVAE(*DEFAULT)
     #vae.load_pretrained("./training/SimpleVAE/run2")
 
-    create_video(SimpleVAE, DEFAULT, "./music/heartbeat.wav", "./training/SimpleVAE/run3",
-                 "heartbeat.mp4", fps=24)
+    #create_video(SimpleVAE, DEFAULT, "./music/heartbeat.wav", "./training/SimpleVAE/run3",
+                 #"heartbeat.mp4", fps=24)
